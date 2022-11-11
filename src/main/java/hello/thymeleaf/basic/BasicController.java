f70685d913436e7b593356e22fa8839f5373668c
ohmyoungjin <sisaudwls@naver.com>
2022-11-12 00:05:40 +0900

    Spring_MVC2 thymeleaf - 연산

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -83,6 +83,14 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "/basic/literal";
     }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
     @Data
     static class User {
         private String username;
