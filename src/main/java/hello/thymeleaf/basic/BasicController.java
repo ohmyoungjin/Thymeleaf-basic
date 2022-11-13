074b77da5226925597ff3619f9e2873cad02eeff
ohmyoungjin <sisaudwls@naver.com>
2022-11-13 22:51:57 +0900

    20221113 :: thymeleaf - javascript inline

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -106,14 +106,6 @@ public class BasicController {
         addUsers(model);
         return "basic/each";
     }
-    private void addUsers(Model model) {
-        List<User> list = new ArrayList<>();
-        list.add(new User("userA", 10));
-        list.add(new User("userB", 20));
-        list.add(new User("userC", 30));
-        model.addAttribute("users", list);
-    }
-
     @GetMapping("/condition")
     public String condition(Model model) {
         addUsers(model);
@@ -132,6 +124,20 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
 
     @Data
     static class User {
